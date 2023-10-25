052d1e5021a9394c915ad1140f1c966ff4c0b2f7
stephangoerigk <stephan.goerigk@gmail.com>
2023-10-25 18:17:30 +0200

    update

--- LMM/LMM.R
+++ LMM/LMM.R
@@ -1,1 +1,27 @@
+library(ggplot2)
+library(lme4)
+library(lmerTest)
+
 d = read.csv(file = "LMM/lmer_data.csv")
+
+names(d)
+
+ggplot(data = d, aes(x = time, y = y, linetype = treatment)) + 
+  #stat_summary() +
+  #stat_summary(geom = "line") +
+geom_smooth(method = "lm")
+
+model = lm(y ~ time * treatment, data = d)
+summary(model)
+
+model_simple = lmer(y ~ time * treatment + (1|subject), data = d)
+model_complex = lmer(y ~ time * treatment + (time|subject), data = d, REML = F)
+
+anova(model_simple, model_complex)
+
+summary(model2)
+anova(model2)
+
+# chi-square-likelihood-ratio-test
+
+
